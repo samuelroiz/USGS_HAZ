b422ac55d0a26d512f4479bd9375211d018ea312
Sam Roiz <sroiz01@gmail.com>
2022-03-03 09:20:55 -0800

    Page should be deployable. Also added images.

---
+++ static/js/config_copy.js
@@ -0,0 +1,4 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoic3ZlbmtyaXN0b2ZmIiwiYSI6ImNrem9ya3UwbTYxc3Yyc3R2YzhvM3V3N3cifQ.noQSOo5aPrOLTeWKOu6s6Q";
+const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
+const url_2 = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json"
